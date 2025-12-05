82908ef2242a919c36c6aa6a768a12947a02c924
redwheelbarrow <redwheelbarrow@valkonis.com>
2025-12-04 23:06:40 -0700

    fix(libzipimpl): Reduce WASM OOM issues with large dependencies (#6990)
        ## Summary
        Reduce the likelihood of large dependencies causing OOM crashes during install
    
        ## Problem
        Zip files are all loaded into WASM memory during pnp/pnpm install. The WASM libzip
        memory is limited to 2gigs. If there are many dependencies, or a few large ones,
        the limit will be reached and the runtime will crash.
    
        ## Solution
        For read-only operations in LibzipImpl:
        - keep zip file buffers in runtime memory space
        - load the zip file buffer into WASM space for the duration of a single operation, then discard
        - provide hint to cache the files based on the size of the zip exceeding 10MB (arbitrary choice)
          to minimize unnecessary reloads of large zips.
    
        This allows more flexibility for yarn users to increase their node-side memory as their
        dependencies grow. It has an insignificant computational cost in most cases.
    
        ## Changes
        - Use a higher order function to ensure read-only operations load the zip on demand
        - Set `filesShouldBeCached` based on size of zip data
    
        ## Testing
        - Unit tests
        - Verified the reported issue #6990 now succeeds

--- packages/yarnpkg-libzip/sources/libzipImpl.ts
+++ packages/yarnpkg-libzip/sources/libzipImpl.ts
@@ -22,52 +22,78 @@ export class LibZipImpl implements ZipImpl {
   private readonly zip: number;
   private readonly listings: Array<string>;
   private readonly symlinkCount: number;
+  private readonly readOnly: boolean;
+  private buffer: Buffer | null;
 
-  public filesShouldBeCached = true;
+  public readonly filesShouldBeCached;
 
   constructor(opts: ZipImplInput) {
-    const buffer = `buffer` in opts
+    this.buffer = `buffer` in opts
       ? opts.buffer
       : opts.baseFs.readFileSync(opts.path);
 
     this.libzip = getInstance();
+    this.readOnly = opts.readOnly === true;
+    this.filesShouldBeCached = this.buffer.byteLength >= 10 * 1024 * 1024; // 10MB
 
-    const errPtr = this.libzip.malloc(4);
-    try {
-      let flags = 0;
-      if (opts.readOnly)
-        flags |= this.libzip.ZIP_RDONLY;
+    [this.zip, this.lzSource, this.listings, this.symlinkCount] = this.withZip((zip, source) => {
+      const entryCount = this.libzip.getNumEntries(zip, 0);
+      const listings = new Array<string>(entryCount);
+      for (let t = 0; t < entryCount; ++t)
+        listings[t] = this.libzip.getName(zip, t, 0);
 
-      const lzSource = this.allocateUnattachedSource(buffer);
+      const symlinkCount = this.libzip.ext.countSymlinks(zip);
+      if (this.symlinkCount === -1)
+        throw this.makeLibzipError(this.libzip.getError(zip));
+
+      return [zip, source, listings, symlinkCount];
+    });
+  }
+
+  /**
+   * Calls the function with a valid zip handle and source for the zip.
+   *
+   * If the instance is read-only, the zip will be transferred into WASM memory
+   * for the duration of the function call, and discarded afterwards.
+   *
+   * If the instance is read-write, the zip will not be freed from WASM until discard is called.
+   */
+  private withZip<T>(fn: (zip: number, source: number) => T): T {
+    const initialized = this.zip !== undefined;
+    if (this.readOnly || !initialized) {
+      const errPtr = this.libzip.malloc(4);
       try {
-        this.zip = this.libzip.openFromSource(lzSource, flags, errPtr);
-        this.lzSource = lzSource;
-      } catch (error) {
-        this.libzip.source.free(lzSource);
-        throw error;
+        const lzSource = this.allocateUnattachedSource(this.buffer!);
+        let zip: number;
+        try {
+          zip = this.libzip.openFromSource(lzSource, this.readOnly ? this.libzip.ZIP_RDONLY : 0, errPtr);
+        } catch (error) {
+          this.libzip.source.free(lzSource);
+          throw error;
+        }
+
+        if (zip === 0) {
+          const error = this.libzip.struct.errorS();
+          this.libzip.error.initWithCode(error, this.libzip.getValue(errPtr, `i32`));
+
+          const err = this.makeLibzipError(error);
+          this.libzip.free(error);
+          this.libzip.source.free(lzSource);
+          throw err;
+        }
+
+        try {
+          return fn(zip, lzSource);
+        } finally {
+          if (this.readOnly) {
+            this.libzip.discard(zip);
+          }
+        }
+      } finally {
+        this.libzip.free(errPtr);
       }
-
-      if (this.zip === 0) {
-        const error = this.libzip.struct.errorS();
-        this.libzip.error.initWithCode(error, this.libzip.getValue(errPtr, `i32`));
-
-        throw this.makeLibzipError(error);
-      }
-    } finally {
-      this.libzip.free(errPtr);
-    }
-
-    const entryCount = this.libzip.getNumEntries(this.zip, 0);
-
-    const listings = new Array<string>(entryCount);
-    for (let t = 0; t < entryCount; ++t)
-      listings[t] = this.libzip.getName(this.zip, t, 0);
-
-    this.listings = listings;
-
-    this.symlinkCount = this.libzip.ext.countSymlinks(this.zip);
-    if (this.symlinkCount === -1) {
-      throw this.makeLibzipError(this.libzip.getError(this.zip));
+    } else {
+      return fn(this.zip, this.lzSource);
     }
   }
 
@@ -80,18 +106,20 @@ export class LibZipImpl implements ZipImpl {
   }
 
   stat(entry: number): Stat {
-    const stat = this.libzip.struct.statS();
+    return this.withZip(zip => {
+      const stat = this.libzip.struct.statS();
 
-    const rc = this.libzip.statIndex(this.zip, entry, 0, 0, stat);
-    if (rc === -1)
-      throw this.makeLibzipError(this.libzip.getError(this.zip));
+      const rc = this.libzip.statIndex(zip, entry, 0, 0, stat);
+      if (rc === -1)
+        throw this.makeLibzipError(this.libzip.getError(zip));
 
-    const size = (this.libzip.struct.statSize(stat) >>> 0);
-    const mtime = (this.libzip.struct.statMtime(stat) >>> 0);
+      const size = (this.libzip.struct.statSize(stat) >>> 0);
+      const mtime = (this.libzip.struct.statMtime(stat) >>> 0);
 
-    const crc = this.libzip.struct.statCrc(stat) >>> 0;
+      const crc = this.libzip.struct.statCrc(stat) >>> 0;
 
-    return {size, mtime, crc};
+      return {size, mtime, crc};
+    });
   }
 
   makeLibzipError(error: number) {
@@ -108,160 +136,178 @@ export class LibZipImpl implements ZipImpl {
   }
 
   setFileSource(target: PortablePath, compression: CompressionData, buffer: Buffer) {
-    const lzSource = this.allocateSource(buffer);
+    return this.withZip(zip => {
+      const lzSource = this.allocateSource(zip, buffer);
 
-    try {
-      const newIndex = this.libzip.file.add(this.zip, target, lzSource, this.libzip.ZIP_FL_OVERWRITE);
-      if (newIndex === -1)
-        throw this.makeLibzipError(this.libzip.getError(this.zip));
+      try {
+        const newIndex = this.libzip.file.add(zip, target, lzSource, this.libzip.ZIP_FL_OVERWRITE);
+        if (newIndex === -1)
+          throw this.makeLibzipError(this.libzip.getError(zip));
 
-      if (compression !== null) {
-        const rc = this.libzip.file.setCompression(this.zip, newIndex, 0, compression[0], compression[1]);
-        if (rc === -1) {
-          throw this.makeLibzipError(this.libzip.getError(this.zip));
+        if (compression !== null) {
+          const rc = this.libzip.file.setCompression(zip, newIndex, 0, compression[0], compression[1]);
+          if (rc === -1) {
+            throw this.makeLibzipError(this.libzip.getError(zip));
+          }
         }
+        return newIndex;
+      } catch (error) {
+        this.libzip.source.free(lzSource);
+        throw error;
       }
-      return newIndex;
-    } catch (error) {
-      this.libzip.source.free(lzSource);
-      throw error;
-    }
+    });
   }
 
   setMtime(entry: number, mtime: number): void {
-    const rc = this.libzip.file.setMtime(this.zip, entry, 0, mtime, 0);
-    if (rc === -1) {
-      throw this.makeLibzipError(this.libzip.getError(this.zip));
-    }
+    return this.withZip(zip => {
+      const rc = this.libzip.file.setMtime(zip, entry, 0, mtime, 0);
+      if (rc === -1) {
+        throw this.makeLibzipError(this.libzip.getError(zip));
+      }
+    });
   }
 
   getExternalAttributes(index: number): [number, number] {
-    const attrs = this.libzip.file.getExternalAttributes(this.zip, index, 0, 0, this.libzip.uint08S, this.libzip.uint32S);
-    if (attrs === -1)
-      throw this.makeLibzipError(this.libzip.getError(this.zip));
+    return this.withZip(zip => {
+      const attrs = this.libzip.file.getExternalAttributes(zip, index, 0, 0, this.libzip.uint08S, this.libzip.uint32S);
+      if (attrs === -1)
+        throw this.makeLibzipError(this.libzip.getError(zip));
 
-    const opsys = this.libzip.getValue(this.libzip.uint08S, `i8`) >>> 0;
-    const attributes = this.libzip.getValue(this.libzip.uint32S, `i32`) >>> 0;
-    return [opsys, attributes];
+      const opsys = this.libzip.getValue(this.libzip.uint08S, `i8`) >>> 0;
+      const attributes = this.libzip.getValue(this.libzip.uint32S, `i32`) >>> 0;
+      return [opsys, attributes];
+    });
   }
 
   setExternalAttributes(index: number, opsys: number, attributes: number): void {
-    const rc = this.libzip.file.setExternalAttributes(this.zip, index, 0, 0, opsys, attributes);
-    if (rc === -1) {
-      throw this.makeLibzipError(this.libzip.getError(this.zip));
-    }
+    return this.withZip(zip => {
+      const rc = this.libzip.file.setExternalAttributes(zip, index, 0, 0, opsys, attributes);
+      if (rc === -1) {
+        throw this.makeLibzipError(this.libzip.getError(zip));
+      }
+    });
   }
 
   locate(name: string): number {
-    return this.libzip.name.locate(this.zip, name, 0);
+    return this.withZip(zip => {
+      return this.libzip.name.locate(zip, name, 0);
+    });
   }
 
   getFileSource(index: number) {
-    const stat = this.libzip.struct.statS();
+    return this.withZip(zip => {
+      const stat = this.libzip.struct.statS();
 
-    const rc = this.libzip.statIndex(this.zip, index, 0, 0, stat);
-    if (rc === -1)
-      throw this.makeLibzipError(this.libzip.getError(this.zip));
+      const rc = this.libzip.statIndex(zip, index, 0, 0, stat);
+      if (rc === -1)
+        throw this.makeLibzipError(this.libzip.getError(zip));
 
-    const size = this.libzip.struct.statCompSize(stat);
-    const compressionMethod = this.libzip.struct.statCompMethod(stat);
-    const buffer = this.libzip.malloc(size);
-
-    try {
-      const file = this.libzip.fopenIndex(this.zip, index, 0, this.libzip.ZIP_FL_COMPRESSED);
-      if (file === 0)
-        throw this.makeLibzipError(this.libzip.getError(this.zip));
-
-      try {
-        const rc = this.libzip.fread(file, buffer, size, 0);
-
-        if (rc === -1)
-          throw this.makeLibzipError(this.libzip.file.getError(file));
-        else if (rc < size)
-          throw new Error(`Incomplete read`);
-        else if (rc > size)
-          throw new Error(`Overread`);
-
-        const memory = this.libzip.HEAPU8.subarray(buffer, buffer + size);
-        const data = Buffer.from(memory);
-
-        return {data, compressionMethod};
-      } finally {
-        this.libzip.fclose(file);
-      }
-    } finally {
-      this.libzip.free(buffer);
-    }
-  }
-
-  deleteEntry(index: number) {
-    const rc = this.libzip.delete(this.zip, index);
-    if (rc === -1) {
-      throw this.makeLibzipError(this.libzip.getError(this.zip));
-    }
-  }
-
-  addDirectory(path: string): number {
-    const index = this.libzip.dir.add(this.zip, path);
-    if (index === -1)
-      throw this.makeLibzipError(this.libzip.getError(this.zip));
-
-    return index;
-  }
-
-  getBufferAndClose() {
-    try {
-      // Prevent close from cleaning up the source
-      this.libzip.source.keep(this.lzSource);
-
-      // Close the zip archive
-      if (this.libzip.close(this.zip) === -1)
-        throw this.makeLibzipError(this.libzip.getError(this.zip));
-
-      // Open the source for reading
-      if (this.libzip.source.open(this.lzSource) === -1)
-        throw this.makeLibzipError(this.libzip.source.error(this.lzSource));
-
-      // Move to the end of source
-      if (this.libzip.source.seek(this.lzSource, 0, 0, this.libzip.SEEK_END) === -1)
-        throw this.makeLibzipError(this.libzip.source.error(this.lzSource));
-
-      // Get the size of source
-      const size = this.libzip.source.tell(this.lzSource);
-      if (size === -1)
-        throw this.makeLibzipError(this.libzip.source.error(this.lzSource));
-
-      // Move to the start of source
-      if (this.libzip.source.seek(this.lzSource, 0, 0, this.libzip.SEEK_SET) === -1)
-        throw this.makeLibzipError(this.libzip.source.error(this.lzSource));
-
+      const size = this.libzip.struct.statCompSize(stat);
+      const compressionMethod = this.libzip.struct.statCompMethod(stat);
       const buffer = this.libzip.malloc(size);
-      if (!buffer)
-        throw new Error(`Couldn't allocate enough memory`);
 
       try {
-        const rc = this.libzip.source.read(this.lzSource, buffer, size);
+        const file = this.libzip.fopenIndex(zip, index, 0, this.libzip.ZIP_FL_COMPRESSED);
+        if (file === 0)
+          throw this.makeLibzipError(this.libzip.getError(zip));
 
-        if (rc === -1)
-          throw this.makeLibzipError(this.libzip.source.error(this.lzSource));
-        else if (rc < size)
-          throw new Error(`Incomplete read`);
-        else if (rc > size)
-          throw new Error(`Overread`);
+        try {
+          const rc = this.libzip.fread(file, buffer, size, 0);
 
-        let result = Buffer.from(this.libzip.HEAPU8.subarray(buffer, buffer + size));
+          if (rc === -1)
+            throw this.makeLibzipError(this.libzip.file.getError(file));
+          else if (rc < size)
+            throw new Error(`Incomplete read`);
+          else if (rc > size)
+            throw new Error(`Overread`);
 
-        if (process.env.YARN_IS_TEST_ENV && process.env.YARN_ZIP_DATA_EPILOGUE)
-          result = Buffer.concat([result, Buffer.from(process.env.YARN_ZIP_DATA_EPILOGUE)]);
+          const memory = this.libzip.HEAPU8.subarray(buffer, buffer + size);
+          const data = Buffer.from(memory);
 
-        return result;
+          return {data, compressionMethod};
+        } finally {
+          this.libzip.fclose(file);
+        }
       } finally {
         this.libzip.free(buffer);
       }
-    } finally {
-      this.libzip.source.close(this.lzSource);
-      this.libzip.source.free(this.lzSource);
-    }
+    });
+  }
+
+  deleteEntry(index: number) {
+    return this.withZip(zip => {
+      const rc = this.libzip.delete(zip, index);
+      if (rc === -1) {
+        throw this.makeLibzipError(this.libzip.getError(zip));
+      }
+    });
+  }
+
+  addDirectory(path: string): number {
+    return this.withZip(zip => {
+      const index = this.libzip.dir.add(zip, path);
+      if (index === -1)
+        throw this.makeLibzipError(this.libzip.getError(zip));
+
+      return index;
+    });
+  }
+
+  getBufferAndClose() {
+    return this.withZip((zip, source) => {
+      try {
+        // Prevent close from cleaning up the source
+        this.libzip.source.keep(source);
+
+        // Close the zip archive
+        if (this.libzip.close(zip) === -1)
+          throw this.makeLibzipError(this.libzip.getError(zip));
+
+        // Open the source for reading
+        if (this.libzip.source.open(source) === -1)
+          throw this.makeLibzipError(this.libzip.source.error(source));
+
+        // Move to the end of source
+        if (this.libzip.source.seek(source, 0, 0, this.libzip.SEEK_END) === -1)
+          throw this.makeLibzipError(this.libzip.source.error(source));
+
+        // Get the size of source
+        const size = this.libzip.source.tell(source);
+        if (size === -1)
+          throw this.makeLibzipError(this.libzip.source.error(source));
+
+        // Move to the start of source
+        if (this.libzip.source.seek(source, 0, 0, this.libzip.SEEK_SET) === -1)
+          throw this.makeLibzipError(this.libzip.source.error(source));
+
+        const buffer = this.libzip.malloc(size);
+        if (!buffer)
+          throw new Error(`Couldn't allocate enough memory`);
+
+        try {
+          const rc = this.libzip.source.read(source, buffer, size);
+
+          if (rc === -1)
+            throw this.makeLibzipError(this.libzip.source.error(source));
+          else if (rc < size)
+            throw new Error(`Incomplete read`);
+          else if (rc > size)
+            throw new Error(`Overread`);
+
+          let result = Buffer.from(this.libzip.HEAPU8.subarray(buffer, buffer + size));
+
+          if (process.env.YARN_IS_TEST_ENV && process.env.YARN_ZIP_DATA_EPILOGUE)
+            result = Buffer.concat([result, Buffer.from(process.env.YARN_ZIP_DATA_EPILOGUE)]);
+
+          return result;
+        } finally {
+          this.libzip.free(buffer);
+        }
+      } finally {
+        this.libzip.source.close(source);
+        this.libzip.source.free(source);
+      }
+    });
   }
 
   private allocateBuffer(content: string | Buffer | ArrayBuffer | DataView) {
@@ -287,25 +333,29 @@ export class LibZipImpl implements ZipImpl {
 
     if (source === 0) {
       this.libzip.free(error);
+      this.libzip.free(buffer);
       throw this.makeLibzipError(error);
     }
 
     return source;
   }
 
-  private allocateSource(content: string | Buffer | ArrayBuffer | DataView) {
+  private allocateSource(zip: number, content: string | Buffer | ArrayBuffer | DataView) {
     const {buffer, byteLength} = this.allocateBuffer(content);
-    const source = this.libzip.source.fromBuffer(this.zip, buffer, byteLength, 0, 1);
+    const source = this.libzip.source.fromBuffer(zip, buffer, byteLength, 0, 1);
 
     if (source === 0) {
       this.libzip.free(buffer);
-      throw this.makeLibzipError(this.libzip.getError(this.zip));
+      throw this.makeLibzipError(this.libzip.getError(zip));
     }
 
     return source;
   }
 
   public discard(): void {
-    this.libzip.discard(this.zip);
+    if (!this.readOnly)
+      this.libzip.discard(this.zip);
+
+    this.buffer = null;
   }
 }
